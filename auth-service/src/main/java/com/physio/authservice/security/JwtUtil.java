17ab67ba9230c0be7ee920ed40aefdc14c66e99e
Diogo Coelho <diogocoelho029@gmail.com>
2026-02-18 23:25:35 +0000

    Added JwtUtil for token generation

---
+++ auth-service/src/main/java/com/physio/authservice/security/JwtUtil.java
@@ -0,0 +1,23 @@
+package com.physio.authservice.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final String jwtSecret = "MySuperSecretKey";
+    private final long jwtExpirationMs = 86400000;
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+}
